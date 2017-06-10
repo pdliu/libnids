2a45f82b5f45c6659210456a137fafc769e2f60e
pdliu <liupeidong@buaa.edu.cn>
2017-06-10 11:32:58 +0800

    To Huawei Style

--- samples/chksum_ctl.c
+++ samples/chksum_ctl.c
@@ -30,22 +30,29 @@ int all_local_ipaddrs_chksum_disable()
     unsigned int tmp;
 
     if (!get_all_ifaces(&ifaces, &ifaces_count))
+    {
         return -1;
-    ctlp =
-        (struct nids_chksum_ctl *) malloc(ifaces_count *
-                          sizeof(struct
-                             nids_chksum_ctl));
+    }
+
+    ctlp = (struct nids_chksum_ctl *) 
+           malloc(ifaces_count * sizeof(struct nids_chksum_ctl));
     if (!ctlp)
+    {
         return -1;
-    for (i = 0; i < ifaces_count; i++) {
+    }
+
+    for (i = 0; i < ifaces_count; i++) 
+    {
         tmp = get_addr_from_ifreq(ifaces + i);
-        if (tmp) {
+        if (tmp)
+        {
             ctlp[ind].netaddr = tmp;
             ctlp[ind].mask = inet_addr("255.255.255.255");
             ctlp[ind].action = NIDS_DONT_CHKSUM;
             ind++;
         }
     }
+
     free(ifaces);
     nids_register_chksum_ctl(ctlp, ind);
 }
@@ -54,8 +61,10 @@ int all_local_ipaddrs_chksum_disable()
 unsigned int get_addr_from_ifreq(struct ifreq *iface)
 {
     if (iface->ifr_addr.sa_family == AF_INET)
-        return ((struct sockaddr_in *) &(iface->ifr_addr))->
-            sin_addr.s_addr;
+    {
+        return ((struct sockaddr_in *) &(iface->ifr_addr))->sin_addr.s_addr;
+    }
+
     return 0;
 }
 
@@ -69,8 +78,12 @@ static int get_all_ifaces(struct ifreq **ifaces, int *count)
     *ifaces = malloc(ifaces_size);
     sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
     if (sock <= 0)
+    {
         return 0;
-    for (;;) {
+    }
+
+    for (;;) 
+    {
         param.ifc_len = ifaces_size;
         param.ifc_req = *ifaces;
         if (ioctl(sock, SIOCGIFCONF, &param))
@@ -81,10 +94,12 @@ static int get_all_ifaces(struct ifreq **ifaces, int *count)
         ifaces_size *= 2;
         ifaces = malloc(ifaces_size);
     }
+
     *count = param.ifc_len / sizeof(struct ifreq);
     close(sock);
     return 1;
-      err:
+
+err:
     close(sock);
     return 0;
 }
